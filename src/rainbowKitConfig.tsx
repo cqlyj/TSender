b3df1b4d120ec6e1dc223f7efb0c884b4413e704
cqlyj <luoyingjie0721@gmail.com>
2025-05-06 21:58:17 +0800

    Add rainbowKit configuration for wallet connection

---
+++ src/rainbowKitConfig.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { getDefaultConfig } from "@rainbow-me/rainbowkit";
+import { anvil, sepolia } from "wagmi/chains";
+export default getDefaultConfig({
+  appName: "TSender",
+  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
+  chains: [sepolia, anvil],
+  ssr: false,
+});
